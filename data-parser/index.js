1f9b3a182c53ce37930cf2a99eda9dad4a6f8b79
Greyson Parrelli <greyson.parrelli@gmail.com>
2018-12-17 20:08:11 -0800

    Give the parse a main() to make it's story more clear.

--- data-parser/index.js
+++ data-parser/index.js
@@ -1,32 +1,33 @@
 const parse = require('csv-parse/lib/sync')
 const fs    = require('fs')
 
-const species  = toMap(parseCsv('species_names.csv'), 'botanical_name')
-const nativity = toMap(parseCsv('species_native_status_EOL_ID.csv'), 'botanical_name')
-const treesRaw = parseCsv('trees.csv')
+function main() {
+  const species  = toMap(parseCsv('species_names.csv'), 'botanical_name')
+  const nativity = toMap(parseCsv('species_native_status_EOL_ID.csv'), 'botanical_name')
+  const treesRaw = parseCsv('trees.csv')
 
-const trees = treesRaw.map(t => {
-  const botanical = t['Name Botanical']
-  return {
-    'name_botanical': botanical,
-    'name_common': t['Name Common'],
-    'family_name_botanical': getOrDefault(species, botanical, 'family_botanical_name', 'Unknown'),
-    'height_group': t['Height Group'],
-    'latitude': t['Latitude'],
-    'longitude': t['Longitude'],
-    'nativity': getOrDefault(nativity, botanical, 'native', 'Unknown'),
-    'eol_id': getOrDefault(nativity, botanical, 'EOL_ID', -1)
-  }
-})
+  const trees = treesRaw.map(t => {
+    const botanical = t['Name Botanical']
+    return {
+      'name_botanical': botanical,
+      'name_common': t['Name Common'],
+      'family_name_botanical': getOrDefault(species, botanical, 'family_botanical_name', 'Unknown'),
+      'height_group': t['Height Group'],
+      'latitude': t['Latitude'],
+      'longitude': t['Longitude'],
+      'nativity': getOrDefault(nativity, botanical, 'native', 'Unknown'),
+      'eol_id': getOrDefault(nativity, botanical, 'EOL_ID', -1)
+    }
+  })
 
-const prefix = 'app.setData('
-const body   = JSON.stringify(trees, null, 2)
-const suffix = ');'
+  const prefix = 'app.setData('
+  const body   = JSON.stringify(trees, null, 2)
+  const suffix = ');'
 
-fs.writeFileSync('data.js', prefix + body + suffix)
+  fs.writeFileSync('data.js', prefix + body + suffix)
 
-console.log('Complete!')
-
+  console.log('Complete!')
+}
 
 function parseCsv(filename) {
   const contents = fs.readFileSync(getPath(filename), 'utf8')
@@ -49,3 +50,5 @@ function toMap(data, key) {
 function getOrDefault(map, key, field, defaultValue) {
   return map[key] && map[key][field] ? map[key][field] : defaultValue
 }
+
+main();
