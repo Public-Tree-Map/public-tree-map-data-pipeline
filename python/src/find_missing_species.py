96fe0002b48fa72ac3c72b6222b9b758c976591e
Mark Jackson <mark@fresnoinc.com>
2019-11-26 00:52:37 -0800

    adding schema check for safty reasons

--- python/src/find_missing_species.py
+++ python/src/find_missing_species.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
 
+species_id_col_name = 'Species ID'
+
 def filter_new_species_ids(trees_inventory_df, species_df):
     return pd.\
         merge(
             trees_inventory_df,
-            species_df['Species ID'],
-            on='Species ID',
+            species_df[species_id_col_name],
+            on=species_id_col_name,
             how='left',
             indicator=True
         ).\
@@ -17,6 +19,11 @@ if __name__ == '__main__':
     species_df = pd.read_csv('data/species_attributes.csv')
     tree_inventory_url = 'https://data.smgov.net/resource/w8ue-6cnd.csv?$limit=50000'
     trees_inventory_df = pd.read_csv(tree_inventory_url)
+
+    if species_id_col_name not in species_df.columns:
+        raise ValueError(f'species_df does not have column: {species_id_col_name}')
+    elif species_id_col_name not in trees_inventory_df.columns:
+        raise ValueError(f'trees_inventory_df does not have column: {species_id_col_name}')
 
     new_species_tree_inventory_df = filter_new_species_ids(trees_inventory_df, species_df)
     print(new_species_tree_inventory_df.to_csv(index=False))
