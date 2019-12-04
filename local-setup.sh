3f249e9ef1b24b1858d815ff67dba777af2d5962
Mark Jackson <mark.jackson@starz.com>
2019-12-04 02:45:55 -0800

    making circleci and local builds/runs play nicely with each other. Also updating python dependencies

--- local-setup.sh
+++ local-setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
-ln -s /public-tree-map/node_modules /public-tree-map-local/node_modules
 
-# run whatever comes next (usually bash)
+# when running locally setup the node dependencies
+if [ -d "/public-tree-map-local" ]; then
+    ln -s /public-tree-map/node_modules /public-tree-map-local/node_modules
+fi
+
+# run whatever command to run (usually bash)
 exec "$@"
