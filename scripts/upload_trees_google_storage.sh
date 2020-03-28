a2667795698ac1c6c9fd58475373854489c4b107
Mark Jackson <mark@fresnoinc.com>
2020-03-28 06:14:23 -0700

    uploading tree files to google storage only on master.

---
+++ scripts/upload_trees_google_storage.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -o errexit  # abort on nonzero exitstatus
+set -o nounset  # abort on unbound variable
+set -o pipefail # don't hide errors within pipes
+
+function upload_google_cloud() {
+  gcloud_service_key="${1}"
+  google_project_id="${2}"
+  echo "${gcloud_service_key}" | gcloud auth activate-service-account --key-file=-
+  gcloud --quiet config set project "${google_project_id}"
+  gsutil cp -Z build/data/map.json gs://public-tree-map/data/
+  gsutil -m cp -Z build/data/trees/*.json gs://public-tree-map/data/trees/
+  gsutil setmeta -h "Cache-Control:public, max-age=43200" gs://public-tree-map/data/map.json
+  gsutil -m setmeta -h "Cache-Control:public, max-age=43200" gs://public-tree-map/data/trees/*.json
+  gsutil -m cp -r build/img gs://public-tree-map/
+}
+
+GCLOUD_SERVICE_KEY="${1:-}"
+GOOGLE_PROJECT_ID="${2:-}"
+BRANCH_NAME="${3}"
+
+if [[ "${BRANCH_NAME}" == 'master' && "${GCLOUD_SERVICE_KEY}" != '' && "${GOOGLE_PROJECT_ID}" != '' ]]; then
+  upload_google_cloud "${GCLOUD_SERVICE_KEY}" "${GOOGLE_PROJECT_ID}"
+else
+  echo "${BRANCH_NAME}" does not support pushing to google storage.
+fi
