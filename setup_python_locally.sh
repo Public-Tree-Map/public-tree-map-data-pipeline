5ffaa771282bdb64f80fe44181b3c09da3e31d86
Mark Jackson <mark.jackson@starz.com>
2020-04-27 22:36:28 -0700

    using requirements.txt file instead

--- setup_python_locally.sh
+++ setup_python_locally.sh
@@ -4,4 +4,5 @@ conda create -n tree-map
 conda activate tree-map
 conda config --env --add channels conda-forge
 conda config --env --set channel_priority strict
-conda install python=3 geopandas==0.4.0
+conda install python=3
+conda install --file requirements.txt
